0cc7095ddf77203585afedac1450a72d17db7796
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-03-03 20:40:13 +0500

    White Screen

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Pygame Test")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255)) 
+    pygame.display.flip()
+
+pygame.quit()
